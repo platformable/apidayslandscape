7d8747c0e84ab6380eee5ce4dfbabfeb5c9e62b0
Alexei Garban <garban.valdeon@gmail.com>
2022-01-11 17:49:02 +0100

    GA added

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react'
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import { CompanyProvider } from '../context/CompanyContext';
+import Script from 'next/script' 
 
 function MyApp({ Component, pageProps }) {
 
@@ -10,6 +11,15 @@ function MyApp({ Component, pageProps }) {
 }, []);
 
   return <>
+  <Script src="https://www.googletagmanager.com/gtag/js?id=UA-59979813-39" strategy="lazyOnload"/>
+  <Script strategy='lazyOnLoad'>
+    {
+        `window.dataLayer = window.dataLayer || []
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+        gtag('config', 'UA-59979813-39')`
+    }
+  </Script>
   <CompanyProvider>
   <Component {...pageProps} />
   </CompanyProvider>

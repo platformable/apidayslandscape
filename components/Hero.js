c1f09a8ac0fece25428a368e37a3de68c722581b
Alexei Garban <garban.valdeon@gmail.com>
2023-11-13 18:13:52 +0100

    hero, toolbar edits

--- components/Hero.js
+++ components/Hero.js
@@ -1,9 +1,9 @@
 export default function Hero() {
   return (
     <section id="hero" className="intro-tex container mx-auto md:py-10 py-2">
-      <div className="text-center">
-        <center><img src="/apilandscape_logo.svg" alt="" className="text-center md:py-10 py-5"/></center>
-        <h3 className="text-[#083ECB] text-center">
+      <div className="text-center md:py-10 py-5">
+        <center><img src="/apilandscape_logo.svg" alt="" className="text-center "/></center>
+        <h3 className="text-[#083ECB] text-center pt-4 pb-2">
           A comprehensive view of all stakeholders creating the programmable
           economy
         </h3>

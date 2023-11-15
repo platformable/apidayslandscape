aa2866160240d1c216b0e897b1d24c8af9ad60fd
Alexei Garban <garban.valdeon@gmail.com>
2023-11-15 13:48:20 +0100

    modal update

--- components/CompanyCard.js
+++ components/CompanyCard.js
@@ -42,75 +42,163 @@ export default function CompanyCard({company,index,handleLoading}) {
   const router = useRouter();
 
 
-  const newParentCategorySlug = [...new Set(company?.parentCategorySlug?.split(","))]
+  const newParentCategorySlug = [...new Set(company?.cluster?.split(","))];
+  const categories = [...new Set(company?.category?.split(","))];
+  const subcategories = [...new Set(company?.subcategory?.split(","))];
     return (
-        <div className="company-card  rounded bg-white" key={index} onClick={() => handleCompany(company)}>
-        <div className="card-top">
-  
-            <div className="card-logo">
-            {company.logo === "" || null ? <img src={`../apidaysReplacementLogo.png`} alt=""  className=""/> : <img srcSet={`${handleImages(company.logo)} 2x`} alt="" className="img-fluid" /> }
-{/*               <img src={handleImages(company.logo)} alt="" onClick={() => handleCompany(company)}/> */}
+      <div className="company-card bg-white" key={index}>
+      <div className="card-top">
+        <div className="">
+          <h3 className="fw-bold text-company-color text-center text-[#243672]">
+            {" "}
+            {company.name}
+          </h3>
+
+          <div className="card-logo flex justify-center">
+            {company.logo === "" || null ? (
+              <img
+                src={`../../../apidaysReplacementLogo.png`}
+                alt=""
+                
+              />
+            ) : (
+              <img
+                srcSet={`${handleImages(company.logo)} 2x`}
+                alt=""
+                
+                
+              />
+            )}
+          </div>
+
+   {/*        <div className="company-decription my-3">
+            <p className=" text-[#243672]">{company.description}</p>
+          </div> */}
+
+          {/* <span className="xs-text badge tex-black">{company?.url?.length>6? "find out more":""}</span> */}
+
+          <div className="flex gap-x-5 items-center mb-3">
+            <div>
+              <img src="/founded_icon.svg" alt="" className="" />
             </div>
-            <div className="card-description">
-              <h6 className="fw-bold text-company-color" onClick={() => handleCompany(company)}> {company.name}</h6>
-              {/* <h6><span className="badge bg-dark-orange text-white">Go Deeper</span></h6> */}
-              {/* <span className="xs-text badge tex-black">{company?.url?.length>6? "find out more":""}</span> */}
-              <div className="card-founded-container">
-                <div className="founded-icon">
-                <img src="../../apilandscape__founded_in_40x40.png" alt="" className="icon m-0 p-0" />
-   
-                </div>
-                <div className="founded-year">
-                <p className="xs-text m-0 p-0 fw-bold">Founded</p>
-                  <p className="xs-text">{company.yearFounded ? `${company.yearFounded}`: "-"}</p>
-                  
-                </div>
-              </div> {/*  <!--card founded--> */}
-              <div className="card-founded-container">
-                <div className="founded-icon">
-                <img src="../../apilandscape__headquarter_40x40.png" alt="" className="icon" />
-                </div>
-                <div className="founded-year">
-                <p className="xs-text m-0 p-0 fw-bold">Headquarter</p>
-                  <p className="xs-text">{company.headquartersCity},{company.headquartersCountry}</p>
-                </div>
-              </div>  {/* <!--card founded--> */}
+            <div>
+              {" "}
+              <p className="md-text m-0 p-0 text-[#083ECB]">Founded</p>
             </div>
-          </div> {/* <!--cardtop--> */}
-        <div className="card-middle mt-1">
-                {newParentCategorySlug?.map((category,index)=>{
-                  return (<span className={`card-middle-tag mb-1 me-1 text-black
-                  ${category.includes("API Lifecycle Platform") && "bg-dark-main-color text-white"}
-                  ${category==="API Standards/Protocols" && "bg-dark-main-color text-white"}
-                  ${category==="Backend Building Tools" && "bg-dark-main-color text-white"}
-                  ${category==="Business processes as an API/API-as a Product" && "bg-dark-main-color text-white"}
-                  ${category==="Business processes as an API/API-as a Products" && "bg-dark-main-color text-white"}
-                  ${category==="Integration Platform as a Service" && "bg-dark-main-color text-white"}
-                  ${category==="Vertical API Abstractions" && "bg-dark-main-color text-white"}
-                  `}>{category}</span>)
-                })}
-   
-              </div>{/* <!--card middle--> */}
-        
-        <div className="card-bottom">
-          <div className="card-bottom-left">
-            <span className="sm-text">Headcount</span>
-            <p className="fw-bold">{company.headcount?company.headcount:"-"}</p>
-            <img src="../apilandscape_headcount_80x50_companies card.png" alt="" className="sd-icon mt-3" />
-            
-          </div>{/*  <!--card-bottom -left--> */}
-          <div className="card-bottom-center">
-             <span>Total Funding</span>
-            <p className="fw-bold ">{company.totalFunding? reduceNumber(company.totalFunding) : "-"}</p>
-            <img src="../../apilandscape_total_funding__60x45.png" alt="" className="md-icon mt-3"/>
-          </div> {/* <!--card-bottom -center--> */}
-          <div className="card-bottom-right">
-             <span>Estimated Revenue</span>
-            <p className="sm-text fw-bold text-center">{company.estimatedRevenueRange?company.estimatedRevenueRange: "-"}</p>
-            <img src="../../money-line.png" alt="" />
-          </div> {/* <!--card-bottom -right--> */}
-        </div> {/* card-bottom */}
-      
-      </div> /* company card */
+            <div>
+              {" "}
+              <p className="font-bold  m-0 p-0 text-[#243672]">
+                {company.yearFounded ? `${company.yearFounded}` : "-"}
+              </p>
+            </div>
+          </div>
+
+          <div className="flex gap-x-5 items-center">
+            <div>
+              <img src="/headquarter_icon.svg" alt="" className="" />
+            </div>
+            <div>
+              {" "}
+              <p className="md-text m-0 p-0  text-[#083ECB]">Founded</p>
+            </div>
+            <div>
+              {" "}
+              <p className="md-text font-bold  m-0 p-0 text-[#243672]">
+                {company.headquartersCity},{company.headquartersCountry}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>{" "}
+      {/* <!--cardtop--> */}
+      <div className="card-middle mt-2">
+ {/*        {newParentCategorySlug?.map((cluster, index) => {
+          return (
+            <span
+              className={`text-center bg-[#98BDFF] my-1 text-[#083ECB] d-block border-[#F69902] orangeBorder`}
+            >
+              {cluster}
+            </span>
+          );
+        })}
+
+        {categories?.map((category, index) => {
+          return (
+            <span
+              key={index}
+              className={`text-center bg-[#E6EFFF] my-1 text-[#083ECB] d-block orangeBorder`}
+            >
+              {category}
+            </span>
+          );
+        })}
+
+        {subcategories?.map((subcategory, index) => {
+          return (
+            <span
+              key={index}
+              className={`text-center bg-[#ffffff] my-1 text-[#083ECB] d-block orangeBorder`}
+            >
+              {subcategory}
+            </span>
+          );
+        })} */}
+      </div>
+      {/* <!--card middle--> */}
+      <div className="grid grid-cols-3 gap-x-2 my-3">
+        <div className="greenBorder rounded-md p-2 flex flex-col items-center">
+          
+          <p className="font-bold my-2 text-[#243672]">
+            {company.headcount ? company.headcount : "-"}
+          </p>
+          <span className="text-[#083ECB]">Headcount</span>
+          <img
+            src="/headcount_icon.svg"
+            alt=""
+            className="sd-icon mt-3"
+          />
+        </div>
+
+        <div className="purpleBorder rounded-md p-2 flex flex-col items-center">
+          
+          <p className="font-bold my-2 text-[#243672]">
+            {company.totalFunding ? reduceNumber(company.totalFunding) : "-"}
+          </p>
+          <span className="text-[#083ECB]">Total Funding</span>
+          <img
+            src="/total funding_icon.svg"
+            alt=""
+            className="md-icon mt-3"
+          />
+        </div>
+
+        <div className="orangeBorder rounded-md p-2  flex flex-col items-center">
+          
+          <p className="font-bold my-2 text-[#243672]">
+        {/*     {handleScore(
+              company.womanInManagement,
+              company.nonWhitePeopleInManagement
+            )} */}---
+          </p>
+          <span className="text-[#083ECB]">Active products</span>
+          <img
+            src="/active products_icon.svg"
+            alt=""
+            className="xd-icon mt-3 px-2"
+          />
+        </div>
+      </div>
+
+      <div className="my-1 grid">
+    
+        <button
+         onClick={()=>handleRoute(selectedEntity)}
+          className="bg-[#083ECB] shadow-md px-10 py-3  text-white uppercase rounded text-center"
+        >
+         View complete profile
+        </button>
+        {/* <span className="text-sm text-gray">Wrong info? suggest </span> */}
+      </div>
+    </div>
     )
 }

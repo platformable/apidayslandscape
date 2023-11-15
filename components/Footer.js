cc44b10b691b9a2f779d3122753183c92a1758f5
leoncan122 <leon.can122@gmail.com>
2023-11-15 13:51:00 +0100

    Update Profile, Footer & Header

--- components/Footer.js
+++ components/Footer.js
@@ -10,12 +10,13 @@ export default function Footer() {
       <div className="container py-14">
         <p className="flex gap-x-1 items-center justify-center mb-0">
           &copy; This website was built for apidays by{" "}
-          <Link href="https://www.platformable.com" target="_blank">
+          <Link href="https://www.platformable.com" target="_blank" className="flex items-center gap-x-1">
             <img
               src="/footer_platformable.png"
               alt=""
               className="max-w-[125px]"
             />
+            <span className="text-white font-semibold">Platformable.com</span>
           </Link>
           proudly{" "}
           <img src="/heart_icon.png" alt="" className="footer-icon" /> based

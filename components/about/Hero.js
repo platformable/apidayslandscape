fe3336a9746806145d2cdb8fbe99590c924fa17e
leoncan122 <leon.can122@gmail.com>
2023-11-21 13:20:56 +0100

    About page update

--- components/about/Hero.js
+++ components/about/Hero.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <section className="bg-[var(--dark-blue)] text-white py-20  text-2xl  border-y-2 border-[var(--orange)]">
+      <article className="container mx-auto leading-relaxed tracking-wide">
+      apidays is the world’s leading API conference series, with 2024 events
+      already organised in 6 cities around the globe: Singapore, New York,
+      Helsinki, London, Melbourne, and Paris, with more to come. <br/>
+      In addition to
+      the conference events, apidays hosts a news website, webinars, podcasts,
+      publishes reports and leads initiatives focused on supporting women in the
+      API industry and encouraging sustainable IT practices in the API sector.
+      </article>
+    </section>
+  );
 }
